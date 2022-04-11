148cfff01d30f9a052e0979461b0d4f908fd57ed
Mathieu <mathieu.dubart@icloud.com>
2022-04-11 21:56:14 +0200

    adding interactjs lib

--- prototypages/scriptProto.js
+++ prototypages/scriptProto.js
@@ -1,3 +1,7 @@
+import interact from 'interactjs'
+
+// -------------- SCRIPT MAP -------------- //
+
 let viewMap = document.getElementById("viewMap");
 let heroContainer = document.getElementById("heroContainer");
 let startButton = document.getElementById("startButton");
@@ -8,3 +12,6 @@ startButton.addEventListener("click", ()=>{
     startButton.style.display = "none";
     console.log("it works");
 })
+
+// -------------- DRAG AND DROP -------------- //
+
