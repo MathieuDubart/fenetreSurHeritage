21e8f66abb13e51b6617ce1e7d4a10fc56fc9218
Mathieu <mathieu.dubart@icloud.com>
2022-04-14 01:20:42 +0200

    fixing scroll behavior

--- script/expScript.js
+++ script/expScript.js
@@ -1,34 +1,40 @@
 // ############################# EXPERIENCES PAGES VIDEO ################################# //
 
 function reduceVideo() {
-    var x = document.getElementsByClassName("background-video");
-    var y = document.getElementsByClassName("videoContainer");
-    
+    var x = document.getElementsByClassName("background-video")[0];
+    var y = document.getElementsByClassName("videoContainer")[0];
+    var logo = document.getElementsByClassName("logoToHomepage")[0];
+
+
+    // PENSER A FAIRE QUERY SLEECTO
     if(window.scrollY > 10){
         if (window.innerWidth > 980){
-            x[0].style.width = "15%";
-            y[0].style.left = "calc(50% - 7.5%)";
-            x[0].style.height = "75%";
-            y[0].style.top = "calc(50vh - 37.5%)";
+            x.style.width = "15%";
+            y.style.left = "calc(50% - 7.5%)";
+            x.style.height = "75%";
+            y.style.top = "calc(50vh - 37.5%)";
+            logo.style.display = "block";
         }  else {
             if(window.scrollY > 10){
-                x[0].style.width = "50%";
-                y[0].style.left = "calc(50% - 25%)";
-                x[0].style.height = "500px";
-                y[0].style.top = "calc(50vh - 250px)";
+                x.style.width = "50%";
+                y.style.left = "calc(50% - 25%)";
+                x.style.height = "500px";
+                y.style.top = "calc(50vh - 250px)";
+                logo.style.display = "block";
             }
         }
-        y[0].style.position = "fixed";
-        y[0].style.float = "center";
+        y.style.position = "fixed";
+        y.style.float = "center";
     }
 
 
     if (window.scrollY <= 10){
-        x[0].style.width = "100%";
-        x[0].style.height = "100%";
-        y[0].style.left ="0";
-        y[0].style.top = "0";
-        x[0].style.position = "relative";
+        x.style.width = "100%";
+        x.style.height = "100%";
+        y.style.left ="0";
+        y.style.top = "0";
+        x.style.position = "relative";
+        logo.style.display = "none";
     }
 }
 
